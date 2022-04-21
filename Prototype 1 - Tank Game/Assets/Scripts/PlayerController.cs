492d5f2b30b33ebba74bd31f5a6ed6e06712401c
Cory <Cannedcornflakes@gmail.com>
2022-04-21 17:51:16 -0600

    Moved Project 1 - Tank Game

---
+++ Prototype 1 - Tank Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    public float turnspeed;
+
+    public float hInput;
+
+    public float vInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      hInput = Input.GetAxis("Horizontal");
+      vInput = Input.GetAxis("Vertical");
+
+      //move the tank left and right
+      transform.Rotate(Vector3.up, turnspeed * hInput * Time.deltaTime);
+      //Move the tank forward and back
+      transform.Translate(Vector3.forward * speed * Time.deltaTime *vInput);
+    }
+}
